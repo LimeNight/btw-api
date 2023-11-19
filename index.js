2e8e439afec82810ef4bc1022d11172e99955a73
SummerDev <ivosummer@gmail.com>
2023-11-19 13:41:48 +0100

    cors update

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const port = process.env.PORT || 5000
 
 // Add middleware
 app.use(express.urlencoded({ extended: false }))
-app.use(cors())
+app.use(cors({
+  credentials: true,
+  origin: '*',
+}))
 app.use(express.json())
 app.use(cookieParser())
 

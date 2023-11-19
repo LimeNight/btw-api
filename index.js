8186d528d68796cba8e9a828d02abd0d72f42f08
SummerDev <ivosummer@gmail.com>
2023-11-19 13:35:02 +0100

    vercel header cors define

--- index.js
+++ index.js
@@ -12,10 +12,7 @@ const port = process.env.PORT || 5000
 
 // Add middleware
 app.use(express.urlencoded({ extended: false }))
-app.use(cors({
-  origin: '*',
-  credentials: true
-}))
+app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 
